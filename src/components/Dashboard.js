c041d0315b69b126e35700db32d4a175c4aa3491
Ivan Juliant <ivanjuliant30@gmail.com>
2020-04-29 21:46:51 +0700

    Data Tables

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,68 +1,64 @@
-import React from 'react';
+import React from "react";
 
 //Card
-import TotalActiveUser from './cardDashBoard/TotalActiveUser';
-import TotalUser from './cardDashBoard/TotalUser';
-import TotalVerifiedUser from './cardDashBoard/TotalVerifiedUser';
-import TotalGenerateWallet from './cardDashBoard/TotalGenerateWallet';
-import TotalRegisterToday from './cardDashBoard/TotalRegisterToday';
-import TotalTransaction from './cardDashBoard/TotalTransaction';
-import TotalActiveWallet from './cardDashBoard/TotalActiveWallets';
-import TotalCodeo from './cardDashBoard/TotalCodeo';
+import TotalActiveUser from "./cardDashBoard/TotalActiveUser";
+import TotalUser from "./cardDashBoard/TotalUser";
+import TotalVerifiedUser from "./cardDashBoard/TotalVerifiedUser";
+import TotalGenerateWallet from "./cardDashBoard/TotalGenerateWallet";
+import TotalRegisterToday from "./cardDashBoard/TotalRegisterToday";
+import TotalTransaction from "./cardDashBoard/TotalTransaction";
+import TotalActiveWallet from "./cardDashBoard/TotalActiveWallets";
+import TotalCodeo from "./cardDashBoard/TotalCodeo";
 
 //Tables
-import Tables from './tableDashboard/index';
+import Tables from "./tableDashboard/index";
 
-
-function Dashboard () {
-    return (
-        <div>
-          <div className="row">
-              <div className="col-sm-12">
-                <div className="page-title-box">
-                  <div className="float-right">
-                    <ol className="breadcrumb">
-                      <li className="breadcrumb-item">
-                        <a href="#admin">Admin</a>
-                      </li>
-                      <li className="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                  </div>
-                  <h4 className="page-title">Dashboard</h4>
-                </div>
-              </div>
+function Dashboard() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="page-title-box">
+            <div className="float-right">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item">
+                  <a href="#admin">Admin</a>
+                </li>
+                <li className="breadcrumb-item active">Dashboard</li>
+              </ol>
+            </div>
+            <h4 className="page-title">Dashboard</h4>
           </div>
-
-        <Cards />
-
-        
-
+        </div>
       </div>
-    )
-};
+
+      <Cards />
+    </div>
+  );
+}
 
 const Cards = () => {
-    return (
-        <>
-            <div className="row">
-                <TotalUser />
-                <TotalActiveUser />
-                <TotalVerifiedUser />
-                <TotalTransaction />
-            </div>
+  return (
+    <>
+      <div className="row">
+        <TotalUser />
+        <TotalActiveUser />
+        <TotalVerifiedUser />
+        <TotalTransaction />
+      </div>
 
-            <div className="row">
-              <TotalGenerateWallet />
-              <TotalRegisterToday />
-              <TotalActiveWallet />
-              <TotalCodeo />
-            </div>
-            {/* Tables */}
-            <div className="row">
-              <Tables />
-            </div>
-        </>
-    )
-}
+      <div className="row">
+        <TotalGenerateWallet />
+        <TotalRegisterToday />
+        <TotalActiveWallet />
+        <TotalCodeo />
+      </div>
+      {/* Tables */}
+      <div className="row">
+        <Tables />
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/components/cardDashBoardLaunchpad/TotalProject.js
+++ src/components/cardDashBoardLaunchpad/TotalProject.js
@@ -1,41 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function TotalProject() {
-    return (
-        <div className="col-lg-4">
-            <CardLaunchpads 
-                upDownClass="mdi mdi-trending-down" /*====== Set Icon Up or Down Total Data ======*/
-                upDownText="text-danger" /*====== Set Color For Icon Up or Down (text-success = Green Color) / (text-danger = Red Color) ======*/
-                percentData="18%" /*====== Total Percent Up or Down Today ======*/
-                textPercent="Down From Last Month" /*====== Text For Up or Down Total Percentage Today ======*/
-                setColor="primary" /*====== Set Color For Shadow or Background ======*/
-                setIcon="dripicons-jewel" /*====== For Change Icon Can Change Here Using Dripicons ======*/
-            />
-      </div>
-    )
+  return (
+    <div className="col-lg-4">
+      <CardLaunchpads
+        upDownClass="" /*====== Set Icon Up or Down Total Data ======*/
+        upDownText="" /*====== Set Color For Icon Up or Down (text-success = Green Color) / (text-danger = Red Color) ======*/
+        percentData="&nbsp;" /*====== Total Percent Up or Down Today ======*/
+        textPercent="&nbsp;" /*====== Text For Up or Down Total Percentage Today ======*/
+      />
+    </div>
+  );
 }
 
 const CardLaunchpads = (props) => {
-    return(
-        <div className="card card-eco">
-            <div className="card-body">
-                <h4 className="title-text mt-0">New Project Request</h4>
-                <div className="d-flex justify-content-between">
-                    <h3 className="text-primary">
-                        0
-                    </h3>
-                    <i className="dripicons-jewel card-eco-icon bg-primary shadow-primary align-self-center" />
-                </div>
-                <p className="mb-0 text-muted text-truncate">
-                    <span className={props.upDownText}>
-                    <i className={props.upDownClass}> </i>
-                    {props.percentData}
-                    </span>{" "}
-                    {props.textPercent}
-                </p>
-            </div>
+  return (
+    <div className="card card-eco">
+      <div className="card-body">
+        <h4 className="title-text mt-0">TOTAL PROJECT</h4>
+        <div className="d-flex justify-content-between">
+          <h3 className="text-danger">0</h3>
+          <i className="dripicons-view-thumb card-eco-icon bg-danger shadow-danger align-self-center" />
+        </div>
+        <p className="mb-0 text-muted text-truncate">
+          <span className={props.upDownText}>
+            <i className={props.upDownClass}> </i>
+            {props.percentData}
+          </span>{" "}
+          {props.textPercent}
+        </p>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default TotalProject
+export default TotalProject;
